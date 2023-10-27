ab62053e09428bffdbe315f188aa5ce7282478c4
dhanushwaran06 <ajdhanush.jvl@gmail.com>
2023-10-27 09:33:55 +0530

    commit

---
+++ youtube/src/components/Buttons.tsx
@@ -0,0 +1,45 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const buttonStyle = cva(["transition-color"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-gray-100"],
+      dark: [
+        "bg-secondary-dark",
+        "hover:bg-secondary-dark-hover",
+        "text-secondary",
+      ]
+    },
+    size: {
+      default: [" rounder", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "item-center",
+        "justify-center",
+        "p-2.5",
+      ],
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+type ButtonProps = VariantProps<typeof buttonStyle> & ComponentProps<"button">;
+
+export function Buttons({ variant, size, className, ...props }: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={twMerge(buttonStyle({ variant, size }), className)}
+    />
+  );
+}
